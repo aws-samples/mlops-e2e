c8006705d29b73a0179098f5e1dfe9bb84f65612
Jessie Wei <jeswei@amazon.com>
2023-04-24 21:49:04 +1000

    build(deps): Update deps version

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,13 +3,15 @@
 set -e
 
 # Bootstrap the MLOps infrastructure
-
 pushd ./infrastructure
 echo 'Installing dependencies'
-yarn
+yarn install --frozen-lockfile
+
+pushd ./infrastructure/functions/dataSourceMonitor
+echo 'Installing dependencies for functions'
+yarn install --frozen-lockfile
+popd
 
 echo 'Deploying Insfrastructure in your AWS account'
 yarn cdk bootstrap
 yarn cdk deploy --require-approval never
-
-popd
