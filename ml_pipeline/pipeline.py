121f941f1f0abe6eb440ac307be6ad55f8fde5cb
shareefyanal <shareef.yanal@gmail.com>
2024-01-29 21:28:04 +0200

    update sklearn path

--- ml_pipeline/pipeline.py
+++ ml_pipeline/pipeline.py
@@ -152,7 +152,7 @@ def get_pipeline(
         instance_type="ml.t3.large",
         role=role,
         sagemaker_session=sagemaker_session,
-        output_path= default_bucket,
+        output_path="s3://mlopsinfrastracturestack--sagemakerconstructsagema-b5kdwdejsosj/",
         hyperparameters={"alpha": 10}
     )
     print("FINISH - SKLEARN")
