b0ee37c23c509584f6dca1502b921380c14fbe7c
Salman Malik <salmanmalik@Salmans-Laptop.local>
2024-07-25 14:43:10 +0500

    fixing paths for pipeline.yml

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -2,33 +2,38 @@
 
 set -e
 
-# Create/Update the SageMaker Pipeline and wait for the execution to be completed
-
-VIRTUAL_ENV=.venv
-DATA_MANIFEST=`cat ./dataManifest.json`
-
+# Navigate to the ml_pipeline directory
 pushd ml_pipeline
 
-# Set up virtual env
-virtualenv -p python3 $VIRTUAL_ENV
-. $VIRTUAL_ENV/bin/activate 
+# Set up the virtual environment in the correct directory
+python3 -m venv .venv
+source .venv/bin/activate 
 
-#Install requirements
-pip3 install -r requirements.txt
+# Upgrade pip to avoid compatibility issues
+pip install --upgrade pip
+
+# Install Cython first
+pip install Cython
+
+# Install requirements
+pip install -r requirements.txt
+
+# Ensure we are in the correct directory before running the pipeline
+cd $CODEBUILD_SRC_DIR/ml_pipeline
 
 echo "Starting Pipeline Execution"
 export PYTHONUNBUFFERED=TRUE
-python3 run_pipeline.py --module-name pipeline \
-        --role-arn $SAGEMAKER_PIPELINE_ROLE_ARN \
-        --tags "[{\"Key\":\"sagemaker:project-name\", \"Value\":\"${SAGEMAKER_PROJECT_NAME}\"}]" \
-        --kwargs "{\"region\":\"${AWS_REGION}\",\"role\":\"${SAGEMAKER_PIPELINE_ROLE_ARN}\",\"default_bucket\":\"${SAGEMAKER_ARTIFACT_BUCKET}\",\"pipeline_name\":\"${SAGEMAKER_PROJECT_NAME}\",\"model_package_group_name\":\"${SAGEMAKER_PROJECT_NAME}\",\"base_job_prefix\":\"${SAGEMAKER_PROJECT_NAME}\"}"
+python run_pipeline.py --module-name pipeline \
+       --role-arn $SAGEMAKER_PIPELINE_ROLE_ARN \
+       --tags "[{\"Key\":\"sagemaker:project-name\", \"Value\":\"${SAGEMAKER_PROJECT_NAME}\"}]" \
+       --kwargs "{\"region\":\"${AWS_REGION}\",\"role\":\"${SAGEMAKER_PIPELINE_ROLE_ARN}\",\"default_bucket\":\"${SAGEMAKER_ARTIFACT_BUCKET}\",\"pipeline_name\":\"${SAGEMAKER_PROJECT_NAME}\",\"model_package_group_name\":\"${SAGEMAKER_PROJECT_NAME}\",\"base_job_prefix\":\"${SAGEMAKER_PROJECT_NAME}\"}"
 
 echo "Create/Update of the SageMaker Pipeline and execution Completed."
 
-# Deactivate virtual envs
+# Deactivate the virtual environment
 deactivate
 
 popd
 
 export MODEL_PACKAGE_NAME=`cat ml_pipeline/pipelineExecutionArn` 
-echo "{\"arn\": \"${MODEL_PACKAGE_NAME}\"}" > pipelineExecution.json
+echo "{\"arn\": \"${MODEL_PACKAGE_NAME}\"}" > ml_pipeline/pipelineExecution.json
