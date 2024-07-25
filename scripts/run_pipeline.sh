2b4f08527900ee9c90dd69632fea2fa141316760
Salman Malik <salmanmalik@Salmans-Laptop.local>
2024-07-25 14:16:24 +0500

    fixing paths for pipeline.yml

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -7,18 +7,18 @@ set -e
 VIRTUAL_ENV=.venv
 DATA_MANIFEST=`cat ./dataManifest.json`
 
-pushd ./ml_pipeline
+pushd ml_pipeline
 
 # Set up virtual env
 virtualenv -p python3 $VIRTUAL_ENV
 . $VIRTUAL_ENV/bin/activate 
 
 #Install requirements
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 echo "Starting Pipeline Execution"
 export PYTHONUNBUFFERED=TRUE
-python run_pipeline.py --module-name pipeline \
+python3 run_pipeline.py --module-name pipeline \
         --role-arn $SAGEMAKER_PIPELINE_ROLE_ARN \
         --tags "[{\"Key\":\"sagemaker:project-name\", \"Value\":\"${SAGEMAKER_PROJECT_NAME}\"}]" \
         --kwargs "{\"region\":\"${AWS_REGION}\",\"role\":\"${SAGEMAKER_PIPELINE_ROLE_ARN}\",\"default_bucket\":\"${SAGEMAKER_ARTIFACT_BUCKET}\",\"pipeline_name\":\"${SAGEMAKER_PROJECT_NAME}\",\"model_package_group_name\":\"${SAGEMAKER_PROJECT_NAME}\",\"base_job_prefix\":\"${SAGEMAKER_PROJECT_NAME}\"}"
