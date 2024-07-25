ecab15ffddd89370a9b45281993818bf7257969b
Salman Malik <salmanmalik@Salmans-Laptop.local>
2024-07-25 12:51:43 +0500

    fixing paths for requirements.txt and pipeline.yml

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -16,11 +16,8 @@ virtualenv -p python3 $VIRTUAL_ENV
 # Upgrade pip to avoid compatibility issues
 pip install --upgrade pip
 
-# Install Cython first
-pip install Cython
-
-#Install requirements
-pip install -r ml_pipeline/requirements.txt
+# Install requirements
+pip install -r requirements.txt
 pip install sagemaker==2.148.0
 
 echo "Starting Pipeline Execution"
